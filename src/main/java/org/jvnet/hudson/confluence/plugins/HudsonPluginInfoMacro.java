f491febd4acc57acd965ad4dae7fa7b903b0bc3d
abayer <abayer@@java.net>
2010-01-25 20:21:57 +0000

    [FIXED HUDSON-5366] Added support for optional overrides for sourceDir and jiraComponent

--- src/main/java/org/jvnet/hudson/confluence/plugins/HudsonPluginInfoMacro.java
+++ src/main/java/org/jvnet/hudson/confluence/plugins/HudsonPluginInfoMacro.java
@@ -31,21 +31,21 @@ public class HudsonPluginInfoMacro extends BaseMacro {
      * @param httpRetrievalService the http retrieval service to use
      */
     public void setHttpRetrievalService(HttpRetrievalService httpRetrievalService) {
-	this.httpRetrievalService = httpRetrievalService;
+        this.httpRetrievalService = httpRetrievalService;
     }
     
     /**
      * non inline, so we return false
      */
     public boolean isInline() {
-	return false;
+        return false;
     }
     
     /**
      * no body supported, so this returns false
      */
     public boolean hasBody() {
-	return false;
+        return false;
     }
     
     /**
@@ -53,14 +53,14 @@ public class HudsonPluginInfoMacro extends BaseMacro {
      * so NO_RENDER is returned
      */
     public RenderMode getBodyRenderMode() {
-	return RenderMode.NO_RENDER;
+        return RenderMode.NO_RENDER;
     }
     
     private String getString(JSONObject o, String prop) throws JSONException {
-	if(o.has(prop))
-	    return o.getString(prop);
-	else
-	    return "n/a";
+        if(o.has(prop))
+            return o.getString(prop);
+        else
+            return "n/a";
     }
 
     /**
@@ -74,118 +74,132 @@ public class HudsonPluginInfoMacro extends BaseMacro {
      * 
      */
     public String execute(Map parameters, String body, RenderContext renderContext) throws MacroException {
-	String pluginId = (String)parameters.get("pluginId");
-	
-	if (pluginId == null) {
-	    pluginId = (String)parameters.get("0");
-	}
-	
-	if (pluginId == null) { 
-	    return "No plugin specified.";
-	}
+        String pluginId = (String)parameters.get("pluginId");
+        
+        if (pluginId == null) {
+            pluginId = (String)parameters.get("0");
+        }
+        
+        if (pluginId == null) { 
+            return "No plugin specified.";
+        }
 
-	try {
-	    HttpResponse response = httpRetrievalService.get("http://hudson-ci.org/update-center.json");
+        String jiraComponent = (String) parameters.get("jiraComponent");
+
+        String sourceDir = (String) parameters.get("sourceDir");
+        
+        try {
+            HttpResponse response = httpRetrievalService.get("http://hudson-ci.org/update-center.json");
             if (response.getStatusCode() != 200) {
                 return subRenderer.render("h4. Plugin Information\n"
-					  + "{warning:title=Cannot Load Update Center}\n"
-					  + "error " + response.getStatusCode() + " loading update-center.json\n"
-					  + "{warning}\n", renderContext);
+                                          + "{warning:title=Cannot Load Update Center}\n"
+                                          + "error " + response.getStatusCode() + " loading update-center.json\n"
+                                          + "{warning}\n", renderContext);
             }
-	    
-	    String rawUpdateCenter = IOUtils.toString(response.getResponse()).trim();
-	    
-	    if (rawUpdateCenter.startsWith("updateCenter.post(")) {
-		rawUpdateCenter = rawUpdateCenter.substring(new String("updateCenter.post(").length());
-	    }
+            
+            String rawUpdateCenter = IOUtils.toString(response.getResponse()).trim();
+            
+            if (rawUpdateCenter.startsWith("updateCenter.post(")) {
+                rawUpdateCenter = rawUpdateCenter.substring(new String("updateCenter.post(").length());
+            }
 
-	    if (rawUpdateCenter.endsWith(");")) {
-		rawUpdateCenter = rawUpdateCenter.substring(0,rawUpdateCenter.lastIndexOf(");"));
-	    }
+            if (rawUpdateCenter.endsWith(");")) {
+                rawUpdateCenter = rawUpdateCenter.substring(0,rawUpdateCenter.lastIndexOf(");"));
+            }
 
-	    JSONObject updateCenter;
-	    
-	    updateCenter = new JSONObject(rawUpdateCenter);
-	    
-	    StringBuilder toBeRendered = null;
-	    for (String pluginKey : JSONObject.getNames(updateCenter.getJSONObject("plugins"))) {
-		if (pluginKey.equals(pluginId)) {
-		    JSONObject pluginJSON = updateCenter.getJSONObject("plugins").getJSONObject(pluginKey);
-		    
-		    String name = getString(pluginJSON, "name"),
-			   releaseTimestamp = getString(pluginJSON, "releaseTimestamp"),
-			   fisheyeBaseUrl = "http://fisheye.hudson-ci.org/search/hudson"
-				+ "/trunk/hudson/plugins/" + name
-				+ "?ql=select%20revisions%20from%20dir%20/trunk/hudson/plugins/"
-				+ name + "%20where%20date%20>%20",
-			   fisheyeEndUrl = "%20group%20by%20changeset"
-				+ "%20return%20csid,%20comment,%20author,%20path";
-		    toBeRendered = new StringBuilder("h4. Plugin Information\n"
-			+ "|| Plugin ID | " + name + " |\n"
-			+ "|| Latest Release | " + getString(pluginJSON, "version") + " |\n"
-			+ "|| Latest Release Date | " + getString(pluginJSON, "buildDate") + " |\n"
-			+ "|| Changes via Fisheye | [In Latest Release|" + fisheyeBaseUrl
-			+ getString(pluginJSON, "previousTimestamp")
-			+ "%20and%20date%20<%20" + releaseTimestamp + fisheyeEndUrl
-			+ "]\n[Since Latest Release|" + fisheyeBaseUrl
-			+ releaseTimestamp + fisheyeEndUrl
-			+ "] |\n|| Maintainer(s) | ");
+            JSONObject updateCenter;
+            
+            updateCenter = new JSONObject(rawUpdateCenter);
+            
+            StringBuilder toBeRendered = null;
+            for (String pluginKey : JSONObject.getNames(updateCenter.getJSONObject("plugins"))) {
+                if (pluginKey.equals(pluginId)) {
+                    JSONObject pluginJSON = updateCenter.getJSONObject("plugins").getJSONObject(pluginKey);
+                    
+                    String name = getString(pluginJSON, "name");
 
-		    StringBuilder devString = new StringBuilder();
-		    if (pluginJSON.has("developers")) {
-			JSONArray devArray = pluginJSON.getJSONArray("developers");
-			for (int i = 0; i < devArray.length(); i++) {
-			    String devName = getString(devArray.getJSONObject(i), "name");
-			    String devId = getString(devArray.getJSONObject(i), "developerId");
-			    String devEmail = getString(devArray.getJSONObject(i), "email");
+                    if (jiraComponent == null) {
+                        jiraComponent = name;
+                    }
+                    
+                    if (sourceDir == null) {
+                        sourceDir = name;
+                    }
 
-			    if (devString.length()>0) {
-				devString.append("\n");
-			    }
+                    String releaseTimestamp = getString(pluginJSON, "releaseTimestamp");
+                    String fisheyeBaseUrl = "http://fisheye.hudson-ci.org/search/hudson"
+                        + "/trunk/hudson/plugins/" + sourceDir
+                        + "?ql=select%20revisions%20from%20dir%20/trunk/hudson/plugins/"
+                        + sourceDir + "%20where%20date%20>%20";
+                    String fisheyeEndUrl = "%20group%20by%20changeset"
+                        + "%20return%20csid,%20comment,%20author,%20path";
+                    
+                    toBeRendered = new StringBuilder("h4. Plugin Information\n"
+                                                     + "|| Plugin ID | " + name + " |\n"
+                                                     + "|| Latest Release | " + getString(pluginJSON, "version") + " |\n"
+                                                     + "|| Latest Release Date | " + getString(pluginJSON, "buildDate") + " |\n"
+                                                     + "|| Changes via Fisheye | [In Latest Release|" + fisheyeBaseUrl
+                                                     + getString(pluginJSON, "previousTimestamp")
+                                                     + "%20and%20date%20<%20" + releaseTimestamp + fisheyeEndUrl
+                                                     + "]\n[Since Latest Release|" + fisheyeBaseUrl
+                                                     + releaseTimestamp + fisheyeEndUrl
+                                                     + "] |\n|| Maintainer(s) | ");
 
-			    if (!devEmail.equals("n/a")) {
-				devString.append("[" + devName + "|mailto:" + devEmail + "]");
-			    }
-			    else {
-				devString.append(devName);
-			    }
+                    StringBuilder devString = new StringBuilder();
+                    if (pluginJSON.has("developers")) {
+                        JSONArray devArray = pluginJSON.getJSONArray("developers");
+                        for (int i = 0; i < devArray.length(); i++) {
+                            String devName = getString(devArray.getJSONObject(i), "name");
+                            String devId = getString(devArray.getJSONObject(i), "developerId");
+                            String devEmail = getString(devArray.getJSONObject(i), "email");
 
-			    devString.append(" (id: " + devId + ")");
-			}
-		    }
+                            if (devString.length()>0) {
+                                devString.append("\n");
+                            }
 
-		    if (devString.length()==0) {
-			devString.append("(not specified)");
-		    }
+                            if (!devEmail.equals("n/a")) {
+                                devString.append("[" + devName + "|mailto:" + devEmail + "]");
+                            }
+                            else {
+                                devString.append(devName);
+                            }
 
-		    toBeRendered.append(devString.toString()).append(" |\n");
+                            devString.append(" (id: " + devId + ")");
+                        }
+                    }
+
+                    if (devString.length()==0) {
+                        devString.append("(not specified)");
+                    }
+
+                    toBeRendered.append(devString.toString()).append(" |\n");
                     
                     toBeRendered.append("|| Issue Tracking | [Open Issues|"
-                        + "http://issues.hudson-ci.org/secure/IssueNavigator.jspa?mode=hide&reset=true&jqlQuery=project+%3D+HUDSON+AND+status+in+%28Open%2C+%22In+Progress%22%2C+Reopened%29+AND+component+%3D+'")
-                        .append(name).append("'] |\n");
-		}
-	    }
+                                        + "http://issues.hudson-ci.org/secure/IssueNavigator.jspa?mode=hide&reset=true&jqlQuery=project+%3D+HUDSON+AND+status+in+%28Open%2C+%22In+Progress%22%2C+Reopened%29+AND+component+%3D+'")
+                        .append(jiraComponent).append("'] |\n");
+                }
+            }
 
-	    if (toBeRendered==null) {
-		toBeRendered = new StringBuilder("h4. Plugin Information\n");
-		toBeRendered.append("|| No Information For This Plugin ||\n");
-	    }
+            if (toBeRendered==null) {
+                toBeRendered = new StringBuilder("h4. Plugin Information\n");
+                toBeRendered.append("|| No Information For This Plugin ||\n");
+            }
 
-	    return subRenderer.render(toBeRendered.toString(), renderContext);
-	}
-	catch (JSONException e) {
-	    return subRenderer.render("h4. Plugin Information\n"
-				      + "{warning:title=Cannot Load Update Center}\n"
-				      + "JSONException: " + e.getMessage() + "\n"
-				      + "{warning}\n", renderContext);
-	}
+            return subRenderer.render(toBeRendered.toString(), renderContext);
+        }
+        catch (JSONException e) {
+            return subRenderer.render("h4. Plugin Information\n"
+                                      + "{warning:title=Cannot Load Update Center}\n"
+                                      + "JSONException: " + e.getMessage() + "\n"
+                                      + "{warning}\n", renderContext);
+        }
 
-	catch (IOException e) {
-	    return subRenderer.render("h4. Plugin Information\n"
-				      + "{warning:title=Cannot Load Update Center}\n"
-				      + "IOException: " + e.getMessage() + "\n"
-				      + "{warning}\n", renderContext);
-	}
+        catch (IOException e) {
+            return subRenderer.render("h4. Plugin Information\n"
+                                      + "{warning:title=Cannot Load Update Center}\n"
+                                      + "IOException: " + e.getMessage() + "\n"
+                                      + "{warning}\n", renderContext);
+        }
     }
     
     
