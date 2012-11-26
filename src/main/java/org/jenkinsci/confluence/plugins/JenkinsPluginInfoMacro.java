e379671d29f01bd8d1e87d08bf4cf9f5e8a0de21
Jerome Lacoste <jerome.lacoste@gmail.com>
2012-11-26 11:18:05 +0100

    Add pull requests link for github hosting

--- src/main/java/org/jenkinsci/confluence/plugins/JenkinsPluginInfoMacro.java
+++ src/main/java/org/jenkinsci/confluence/plugins/JenkinsPluginInfoMacro.java
@@ -169,10 +169,13 @@ public class JenkinsPluginInfoMacro extends BaseMacro {
                                     .br().append(getDependencies(updateCenter, pluginJSON))
 
 
-                                    .append(" || Source Code \\\\ Issue Tracking \\\\ Maintainer(s) | ")
+                                    .append(" || Source Code \\\\ Issue Tracking ").append(isGithub?"\\\\ Pull Requests ": "").append("\\\\ Maintainer(s) | ")
                                     .append(isGithub ? "[GitHub|https://github.com/jenkinsci/" : "[Subversion|https://svn.jenkins-ci.org/trunk/hudson/plugins/").append(sourceDir).append(']')
                                     .br().append("[Open Issues|http://issues.jenkins-ci.org/secure/IssueNavigator.jspa?mode=hide&reset=true&jqlQuery=project+%3D+JENKINS+AND+status+in+%28Open%2C+%22In+Progress%22%2C+Reopened%29+AND+component+%3D+%27").append(jiraComponent).append("%27]")
                                     .br();
+                        if (isGithub) {
+                            toBeRendered.href("Pull Requests", "https://github.com/jenkinsci/"+sourceDir+"/pulls").br();
+                        }
 
                         WikiWriter devString = new WikiWriter();
                         if (pluginJSON.has("developers")) {
