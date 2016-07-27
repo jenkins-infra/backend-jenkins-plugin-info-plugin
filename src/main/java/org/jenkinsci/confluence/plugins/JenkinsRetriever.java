5c58238fd596d4767f57497b55b219cac9988c31
Daniel Beck <daniel-beck@users.noreply.github.com>
2016-07-28 00:00:04 +0200

    Back to HTTP

--- src/main/java/org/jenkinsci/confluence/plugins/JenkinsRetriever.java
+++ src/main/java/org/jenkinsci/confluence/plugins/JenkinsRetriever.java
@@ -17,7 +17,7 @@ public class JenkinsRetriever {
 			HttpRetrievalService httpRetrievalService) throws IOException,
 			PluginHttpException, ParseException {
 		HttpResponse response = httpRetrievalService
-				.get("https://updates.jenkins-ci.org/update-center.json");
+				.get("http://updates.jenkins-ci.org/update-center.json");
 		if (response.getStatusCode() != 200) {
 			throw new PluginHttpException(response.getStatusCode());
 		}
